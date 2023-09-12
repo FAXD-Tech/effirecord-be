23cb14d40cf606ff0c45cf9b9712e09a0cf8482b
Li Deng <li.deng129@gamil.com>
2023-09-12 23:08:09 +1000

    init

---
+++ src/main/java/com/faxd/effirecord/EffiRecordApplication.java
@@ -0,0 +1,11 @@
+package com.faxd.effirecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffiRecordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EffiRecordApplication.class, args);
+    }
+}
---
+++ src/test/java/com/faxd/effirecord/EffiRecordApplicationTests.java
@@ -0,0 +1,14 @@
+package com.faxd.effirecord;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EffiRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
