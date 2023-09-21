1f3e594c5b6d02872e6cd8091eda28e8fdd032f4
Li Deng <li.deng129@gamil.com>
2023-09-21 12:01:07 +1000

    fix: change class name Employees to Employee

--- src/main/java/com/faxd/effirecord/mapper/EmployeeMapper.java
+++ src/main/java/com/faxd/effirecord/mapper/EmployeeMapper.java
@@ -4,7 +4,7 @@ package com.faxd.effirecord.mapper;
 import com.faxd.effirecord.dto.employee.EmployeeInfoDto;
 import com.faxd.effirecord.dto.employee.EmployeePostDto;
 import com.faxd.effirecord.dto.employee.EmployeePutDto;
-import com.faxd.effirecord.model.Employees;
+import com.faxd.effirecord.model.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EmployeeMapper {
-    Employees employeePostDtoToEmployees(EmployeePostDto employeePostDto);
+    Employee employeePostDtoToEmployee(EmployeePostDto employeePostDto);
 
-    Employees employeePutDtoToEmployees(EmployeePutDto employeePutDto);
+    Employee employeePutDtoToEmployee(EmployeePutDto employeePutDto);
 
-    EmployeeInfoDto employeesToEmployInfoDto(Employees employees);
+    EmployeeInfoDto employeeToEmployInfoDto(Employee employees);
 
 
 }
--- src/main/java/com/faxd/effirecord/model/Employee.java
+++ src/main/java/com/faxd/effirecord/model/Employee.java
@@ -1,6 +1,7 @@
 package com.faxd.effirecord.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,13 +10,19 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Entity
@@ -25,7 +32,7 @@ import java.time.OffsetDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Employees {
+public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -40,17 +47,21 @@ public class Employees {
     private String password;
 
     @CreationTimestamp
-    private OffsetDateTime createdTime;
+    private LocalDateTime createdTime;
 
     @UpdateTimestamp
-    private OffsetDateTime updatedTime;
+    private LocalDateTime updatedTime;
 
     @Column(name = "is_verified")
     @Builder.Default
     private boolean isVerified = false;
 
-
+    @Column(name = "is_deleted")
     @Builder.Default
     private Boolean isDeleted = false;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "current_project_id")
+    @JsonBackReference
+    private Project projects;
 }
