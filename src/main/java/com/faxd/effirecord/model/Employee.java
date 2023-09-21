f715b8a87876317936def5b4403e438ffeeadf3a
Li Deng <li.deng129@gamil.com>
2023-09-21 12:03:40 +1000

    fix: change class name Employees to Employee

--- src/main/java/com/faxd/effirecord/model/Employee.java
+++ src/main/java/com/faxd/effirecord/model/Employee.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -19,11 +18,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.List;
-
 
 @Entity
 @Table(name = "employees")
