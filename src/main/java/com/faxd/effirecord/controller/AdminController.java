678898bd56fdffd8231acf93c06c21465d7f2538
Li Deng <li.deng129@gamil.com>
2023-09-27 17:42:00 +1000

    feat: add swagger

--- src/main/java/com/faxd/effirecord/controller/AdminController.java
+++ src/main/java/com/faxd/effirecord/controller/AdminController.java
@@ -2,10 +2,6 @@ package com.faxd.effirecord.controller;
 
 import com.faxd.effirecord.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
