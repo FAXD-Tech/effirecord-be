b47319f39adaae3c323f90aa932ebaf1fd0404db
Li Deng <li.deng129@gamil.com>
2023-10-09 00:18:26 +1100

    refactor: extracted same fields from entity into BaseEntity and all entity extends it

--- src/main/java/com/faxd/effirecord/model/BaseEntity.java
+++ src/main/java/com/faxd/effirecord/model/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.faxd.effirecord.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
