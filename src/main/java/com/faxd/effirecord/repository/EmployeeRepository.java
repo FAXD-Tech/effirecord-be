fa090713dc960267bc4d2291db1753673fdf3972
Li Deng <li.deng129@gamil.com>
2023-10-09 00:20:17 +1100

    refactor: extracted same methods of entity jpa repository

--- src/main/java/com/faxd/effirecord/repository/EmployeeRepository.java
+++ src/main/java/com/faxd/effirecord/repository/EmployeeRepository.java
@@ -1,11 +1,7 @@
 package com.faxd.effirecord.repository;
 
-import com.faxd.effirecord.model.BaseEntity;
 import com.faxd.effirecord.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends BaseVerifiedRepository<Employee, Long> {
--- src/main/java/com/faxd/effirecord/repository/ProjectRepository.java
+++ src/main/java/com/faxd/effirecord/repository/ProjectRepository.java
@@ -3,8 +3,6 @@ package com.faxd.effirecord.repository;
 import com.faxd.effirecord.model.Project;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProjectRepository extends BaseVerifiedRepository<Project, Long> {
 
